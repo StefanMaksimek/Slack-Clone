14e4a16672bd127730568536ea514689b31a9ed7
StefanMaksimek <stefan.maksimek@gmail.com>
2022-11-11 11:33:04 +0100

    update output

--- src/app/output/output.component.ts
+++ src/app/output/output.component.ts
@@ -3,19 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-output',
   templateUrl: './output.component.html',
-  styleUrls: ['./output.component.scss']
+  styleUrls: ['./output.component.scss'],
 })
 export class OutputComponent implements OnInit {
+  user = [
+    {
+      userProfilePic: '',
+      userName: '',
+      userMessage: [
+        { text: 'jklfdbnjkndfjkvbnfdj', timeStap: 6545545566 },
+        { text: 'dfvbdf', timeStap: 65455454566 },
+        { text: '', timeStap: 6545547566 },
+        { text: '', timeStap: 6545544566 },
+      ],
+    },
+    {
+      userProfilePic: '',
+      userName: '',
+      userMessage: '',
+    },
+  ];
 
-  user = {
-    "userProfilePic": "",
-    "userName": "",
-    "userMessage": ""
-  }
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
