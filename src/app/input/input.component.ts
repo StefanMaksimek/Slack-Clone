b5a9ef4821deb73038f4f5279cf827adf49c5bde
StefanMaksimek <stefan.maksimek@gmail.com>
2022-11-12 10:26:14 +0100

    update input

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
 })
 export class InputComponent implements OnInit {
+  @ViewChild('holder') holder!: ElementRef;
+  @ViewChild('form') form!: any;
+  @ViewChild('input') input!: any;
 
-  constructor() { }
+  formatting: boolean = true;
+  focusing: boolean = false;
+  constructor(private renderer: Renderer2) {
+    this.renderer.listen('window', 'click', (e: any) => {
+      console.log('path', e.path);
 
-  ngOnInit(): void {
+      if (
+        e.target !== this.holder &&
+        e.target !== this.input &&
+        e.target !== this.form
+      ) {
+        console.log('click outside');
+        this.focusing = false;
+      }
+    });
   }
 
+  ngOnInit(): void {}
+
+  toggleFormatting() {
+    this.formatting = !this.formatting;
+  }
+
+  focusOn() {
+    this.focusing = true;
+    console.log('cklic inside');
+  }
 }
