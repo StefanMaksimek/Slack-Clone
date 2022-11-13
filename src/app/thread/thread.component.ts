6b51eaa995b089e02a29e0ccbe64f3f21c16aab6
StefanMaksimek <stefan.maksimek@gmail.com>
2022-11-13 12:20:14 +0100

    update thread

--- src/app/thread/thread.component.ts
+++ src/app/thread/thread.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { collectionData, Firestore } from '@angular/fire/firestore';
+import { collection } from 'firebase/firestore';
 
 @Component({
   selector: 'app-thread',
   templateUrl: './thread.component.html',
-  styleUrls: ['./thread.component.scss']
+  styleUrls: ['./thread.component.scss'],
 })
 export class ThreadComponent implements OnInit {
-
-  constructor() { }
+  threads: any;
+  constructor(private firestore: Firestore) {}
 
   ngOnInit(): void {
+    this.getThreads();
   }
 
+  getThreads() {
+    collectionData(collection(this.firestore, 'threads')).subscribe(
+      (threads) => {
+        this.threads = threads;
+        console.log('threads', this.threads);
+      }
+    );
+  }
 }
